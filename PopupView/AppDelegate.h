8077368781b269114d6d908f1310ce5c85f02ccb
kaoru <dated13th@gmail.com>
2013-06-09 15:56:03 +0900

    Initial Commit

---
+++ PopupView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PopupView
+//
+//  Created by かおる on 2013/06/09.
+//  Copyright (c) 2013年 かおる. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PopupView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PopupView
+//
+//  Created by かおる on 2013/06/09.
+//  Copyright (c) 2013年 かおる. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
